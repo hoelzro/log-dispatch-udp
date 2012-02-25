94917a455634e310c3f7758573fb39465469864c
Rob Hoelz <rob@hoelz.ro>
2012-02-25 01:14:27 -0600

    initial commit

---
+++ lib/Log/Dispatch/UDP.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Log::Dispatch::UDP;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:  A short description of Log::Dispatch::UDP
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
